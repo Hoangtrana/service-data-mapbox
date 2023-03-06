17d40c6dd3800fe51b8e32a0b2c229767f7769fd
HoangTran <77385339+Hoangtrana@users.noreply.github.com>
2023-03-06 08:39:21 +0200

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>Blancco Frontend Challenge - 2023 &#169; Hoang Tran</p>
+     
+    </div>
+  )
+}
